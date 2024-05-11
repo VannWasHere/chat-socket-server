d63af49fcd38322f3e6800cbf16ccd7c7f155472
VannWasHere <ateen1203@gmail.com>
2024-05-11 20:03:05 +0700

    Change NOTES hehe

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-      origin: "*", // Web Test 
+      origin: "*", // Allow All Source 
       methods: ["GET", "POST"],
       transports: ['websocket', 'polling'],
       credentials: true,
