2bcf7713422cf3b6359868a0bc971c1997edb0b7
VannWasHere <ateen1203@gmail.com>
2024-05-11 12:52:48 +0700

    Added some require socket event

--- server.js
+++ server.js
@@ -19,11 +19,52 @@ let numUsers = 0;
 io.on('connection', (socket) => {
     let addedUser = false;
 
+    // Add messages
     socket.on('New Messsage', (data) => {
         socket.broadcast.emit('New Messsage', {
             username: socket.username,
             message: data
         })
     })
+    
+    // User Join (?)
+    socket.on('Add User', (username) => {
+        if(addedUser) return;
 
+        socket.username = username
+        ++numUsers
+        addedUser = true
+        socket.emit('Login', {
+            numUsers: numUsers
+        })
+    })
+
+    // Notify New User Join
+    socket.broadcast.emit('User Joined', {
+        username: socket.username,
+        numUsers: numUsers
+    })
+
+    socket.on('Typing', () => {
+        socket.broadcast.emit('Typing', {
+            username: socket.username
+        })
+    })
+
+    socket.on('Stop Typing', () => {
+        socket.broadcast.emit('Stop Typing', {
+            username: socket.username
+        })
+    })
+
+    socket.on('disconnect', () => {
+        if(addedUser) {
+            --numUsers
+
+            socket.broadcast.emit('User Left', {
+                username: socket.username,
+                numUsers: numUsers
+            })
+        }
+    })
 });
